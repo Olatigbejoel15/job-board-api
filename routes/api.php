2b55114950be246b994afab1a430a27bda19bde6
Olatigbejoel15 <157637695+Olatigbejoel15@users.noreply.github.com>
2026-02-26 11:55:20 +0100

    Authentication API with Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
-
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/test', function () {
-    return response()->json([
-        'message' => 'Job Board API working'
-    ]);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
+    return $request->user();
 });
